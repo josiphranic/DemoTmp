3dd8f03ce0aba228e418d13b4e90e058cd6cc16d
Josip Hranic <josip.hranic@superology.com>
2022-04-26 21:19:17 +0200

    first commit

--- DemoTmp/ViewController.swift
+++ DemoTmp/ViewController.swift
@@ -1,19 +1,65 @@
 //
 //  ViewController.swift
-//  DemoTmp
+//  Demo1
 //
 //  Created by Josip Hranic on 26.04.2022..
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let tableView = UITableView()
+    private let data: [Int: [String]] = [0 : ["Jason", "Alex", "Sloan"],
+                                         1: ["Jimmy", "Brian", "Kristina"],
+                                         2: ["Pavel", "Naima", "Ali"]]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSubviews()
+        setupTableView()
+        layout()
     }
-
-
 }
 
+private extension ViewController {
+
+    func addSubviews() {
+        view.addSubview(tableView)
+    }
+
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    func layout() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data[section]?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = data[indexPath.section]?[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Section \(section) 😁" // I don't see section names from google meet comment any more so I will write this 😁
+    }
+}
